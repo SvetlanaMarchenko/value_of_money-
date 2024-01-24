bf49e965acfd344a8b054f3dd1686f6c03f48b24
SvetlanaMarchenko <semusv@mail.ru>
2024-01-24 17:39:25 +0200

    Created Personal info page. WIP

---
+++ src/contactInfo.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const ContactInfo = () => {
+  return (
+    <div className="blue_box">
+      <h1>Hello!</h1>
+      <p>
+  My name is <a href="https://www.linkedin.com/in/svetlana-marchenko-6598b8a9/" target="_blank" rel="noopener noreferrer">Svetlana Marchenko</a> and this is my first web project. And any IT project.
+</p>
+
+
+
+
+
+<p>If you need a Junior full stuck developer or you have practice for me - here is my page on <a href="https://www.linkedin.com/in/svetlana-marchenko-6598b8a9/" target="_blank" rel="noopener noreferrer">LinkedIn:</a> </p>
+      <a href="https://www.linkedin.com/in/svetlana-marchenko-6598b8a9/" target="_blank" rel="noopener noreferrer">
+        LinkedIn Profile
+      </a>
+    </div>
+  );
+};
+
+export default ContactInfo;
