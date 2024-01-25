19f3a2bd5a431c4d6f55d0bdbf2b07ef1201ca5a
SvetlanaMarchenko <semusv@mail.ru>
2024-01-25 13:23:24 +0200

    Trying to create a normal structure of navigation with header and body + trying to create a new background. WIP

--- src/App.js
+++ src/App.js
@@ -6,27 +6,16 @@ import MainInfoForm from './MainInfoForm.js';
 import CurrencySelector from './buttonGroup.js';
 import kuva from './earth.png';
 import ContactInfo from './contactInfo.js';
+import vuori from './vuori.png';
 import './App.css';
 
-const Home = () => <h2>Home Page</h2>; // Добавил текст для компонента Home
+// const Home = () => <h2>Home Page</h2>; // Добавил текст для компонента Home
 const Resource = () => <h2>Resource</h2>;
 // const Contact = () => <h2>Contact Page</h2>;
 
-const MyGridComponent = () => {
-  return (
-    <Container>
-      <Row>
-        <Col md={6}>Колонка 1</Col>
-        <Col md={6}>Колонка 2</Col>
-      </Row>
-    </Container>
-  );
-};
-
-
 const BlueBox = ({ children }) => {
   return (
-    <div className="blue_box">
+    <div  className="blue_box" >
       {children}
     </div>
   );
@@ -34,30 +23,27 @@ const BlueBox = ({ children }) => {
 
 
 const App = () => {
+  
   return (
     <div className="App">
-      
-  
       <Router>
         <header className="App-header" style={{ backgroundImage: `url(${kuva})` }}>
           <NavbarComponent />
+
           <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path="/" element={
+              <>
+                <BlueBox>
+                  <h1>Value of money now</h1>
+                  <CurrencySelector/>
+                  <MainInfoForm/>
+                  <Button className='btn_clear'>Clear</Button>
+                </BlueBox>
+              </>
+            } />
             <Route path="/resource" element={<Resource />} />
-            <Route path="/contact" element={<ContactInfo />} />
+            <Route  path="/contact"  style={{ backgroundImage: `url(${vuori})` }} element={<ContactInfo />} />
           </Routes>
-          <BlueBox>
-            <h1>Value of money now</h1>
-            <CurrencySelector/>
-            <MainInfoForm/>
-            <Button className='btn_clear'>Clear</Button>
-
-          </BlueBox>
-          <MyGridComponent />
-
-          
-          
-  
         </header>
       </Router>
     </div>
@@ -65,3 +51,4 @@ const App = () => {
 };
 
 export default App;
+
--- src/contactInfo.js
+++ src/contactInfo.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import vuori from './vuori.png';
 
 const ContactInfo = () => {
   return (
-    <div className="blue_box">
+    <div div style={{ backgroundImage: `url(${vuori})`, backgroundSize: 'cover', height: '100vh' }}>
       <h1>Hello!</h1>
       <p>
   My name is <a href="https://www.linkedin.com/in/svetlana-marchenko-6598b8a9/" target="_blank" rel="noopener noreferrer">Svetlana Marchenko</a> and this is my first web project. And any IT project.
