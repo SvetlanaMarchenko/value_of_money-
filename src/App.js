2a5e5650afb624384cfb5397732b7c1bfdf0b0d3
SvetlanaMarchenko <semusv@mail.ru>
2024-01-25 14:52:05 +0200

    BackgroundImage with CONTACTINFO is changing and working good

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { Container, Row, Col, Button } from 'react-bootstrap';
+import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
 import NavbarComponent from './NavbarComponent.js';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import MainInfoForm from './MainInfoForm.js';
 import CurrencySelector from './buttonGroup.js';
 import kuva from './earth.png';
@@ -9,46 +8,45 @@ import ContactInfo from './contactInfo.js';
 import vuori from './vuori.png';
 import './App.css';
 
-// const Home = () => <h2>Home Page</h2>; // Добавил текст для компонента Home
-const Resource = () => <h2>Resource</h2>;
-// const Contact = () => <h2>Contact Page</h2>;
-
 const BlueBox = ({ children }) => {
   return (
-    <div  className="blue_box" >
+    <div className="blue_box">
       {children}
     </div>
   );
 };
 
+const AppContent = () => {
+  const location = useLocation();
 
-const App = () => {
-  
   return (
     <div className="App">
-      <Router>
-        <header className="App-header" style={{ backgroundImage: `url(${kuva})` }}>
-          <NavbarComponent />
-
-          <Routes>
-            <Route path="/" element={
-              <>
-                <BlueBox>
-                  <h1>Value of money now</h1>
-                  <CurrencySelector/>
-                  <MainInfoForm/>
-                  <Button className='btn_clear'>Clear</Button>
-                </BlueBox>
-              </>
-            } />
-            <Route path="/resource" element={<Resource />} />
-            <Route  path="/contact"  style={{ backgroundImage: `url(${vuori})` }} element={<ContactInfo />} />
-          </Routes>
-        </header>
-      </Router>
+      <header className="App-header" style={{ backgroundImage: location.pathname === '/contact' ? `url(${vuori})` : `url(${kuva})` }}>
+        <NavbarComponent />
+        <Routes>
+          <Route path="/" element={
+            <>
+              <BlueBox>
+                <h1>Value of money now</h1>
+                <CurrencySelector />
+                <MainInfoForm />
+              </BlueBox>
+            </>
+          } />
+          <Route path="/resource" element={<h2>Resource</h2>} />
+          <Route path="/contact" element={<ContactInfo />} />
+        </Routes>
+      </header>
     </div>
   );
 };
 
-export default App;
+const App = () => {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
+  );
+};
 
+export default App;
