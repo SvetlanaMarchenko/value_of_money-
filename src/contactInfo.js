e4d11e532c5a7e15747aa5c4fab704ab9bbe92fc
SvetlanaMarchenko <semusv@mail.ru>
2024-01-29 13:58:31 +0200

    Added new styles for project.WIP

--- src/contactInfo.js
+++ src/contactInfo.js
@@ -1,22 +1,19 @@
 import React from 'react';
-import vuori from './vuori.png';
 
 const ContactInfo = () => {
   return (
-    <div div style={{ backgroundImage: `url(${vuori})`, backgroundSize: 'cover', height: '100vh' }}>
+    <div>
       <h1>Hello!</h1>
-      <p>
-  My name is <a href="https://www.linkedin.com/in/svetlana-marchenko-6598b8a9/" target="_blank" rel="noopener noreferrer">Svetlana Marchenko</a> and this is my first web project. And any IT project.
-</p>
+      <p className='name_diraction'>
+        My name is <a href="https://www.linkedin.com/in/svetlana-marchenko-6598b8a9/" target="_blank" rel="noopener noreferrer">Svetlana Marchenko</a> and this is my first web project. And any IT project.
+      </p>
 
-
-
-
-
-<p>If you need a Junior full stuck developer or you have practice for me - here is my page on <a href="https://www.linkedin.com/in/svetlana-marchenko-6598b8a9/" target="_blank" rel="noopener noreferrer">LinkedIn:</a> </p>
+      <p>If you need a Junior full-stack developer or you have practice for me - here is my page on <a href="https://www.linkedin.com/in/svetlana-marchenko-6598b8a9/" target="_blank" rel="noopener noreferrer">LinkedIn:</a> </p>
       <a href="https://www.linkedin.com/in/svetlana-marchenko-6598b8a9/" target="_blank" rel="noopener noreferrer">
         LinkedIn Profile
       </a>
+
+      <div className="center-line"></div>
     </div>
   );
 };
