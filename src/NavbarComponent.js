7af5e4e9b68ce11f017d48136a7874945984df92
SvetlanaMarchenko <semusv@mail.ru>
2024-02-26 13:56:19 +0200

    Custom_navbar looks exact how i want!

--- src/NavbarComponent.js
+++ src/NavbarComponent.js
@@ -1,26 +1,3 @@
-// import React from 'react';
-// import { Navbar, Nav } from 'react-bootstrap';
-// import { Link } from 'react-router-dom';
-// import './App.css';
-
-// const NavbarComponent = () => {
-//   return (
-//     <Navbar bg="light" variant="light" expand="lg" className="custom_navbar">
-//       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-//       <Navbar.Collapse id="basic-navbar-nav">
-//         <Nav className="mr-auto">
-//           <Nav.Link as={Link} to="/" className="nav_part">Home</Nav.Link>
-//           <Nav.Link as={Link} to="/Resource" className="nav_part">Resource</Nav.Link>
-//           <Nav.Link as={Link} to="/contact" className="nav_part">Contact</Nav.Link>
-//         </Nav>
-//       </Navbar.Collapse>
-//     </Navbar>
-//   );
-// };
-
-// export default NavbarComponent;
-
-
 import React, { useState } from 'react';
 import { Navbar, Nav, Offcanvas } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -39,41 +16,30 @@ const NavbarComponent = () => {
 
   return (
     <>
-    <Navbar bg="light" variant="light" expand="lg" className="custom_navbar">
-      <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggleOffcanvas} />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Navbar.Brand>{selectedNavItem}</Navbar.Brand>
-        <Nav className="mr-auto">
-          <Nav.Link as={Link} to="/" className="nav_part">Home</Nav.Link>
-          <Nav.Link as={Link} to="/Resource" className="nav_part">Resource</Nav.Link>
-          <Nav.Link as={Link} to="/contact" className="nav_part">Contact</Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+      <Navbar bg="light" variant="light" expand="lg" className="custom_navbar">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" onClick={handleToggleOffcanvas} />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Navbar.Brand>{selectedNavItem}</Navbar.Brand>
+          <Nav className="mr-auto">
+            <Nav.Link as={Link} to="/" className="nav_part">Home</Nav.Link>
+            <Nav.Link as={Link} to="/Resource" className="nav_part">Resource</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className="nav_part">Contact</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
 
-
-
-    <Offcanvas show={showOffcanvas} onHide={() => setShowOffcanvas(false)}>
-      <Offcanvas.Header closeButton>
-      </Offcanvas.Header>
-      <Offcanvas.Body>
-        <Nav className="flex-column">
-          <Nav.Link as={Link} to="/" className="nav_part" onClick={() => handleLinkClick('Home')}>Home</Nav.Link>
-          <Nav.Link as={Link} to="/Resource" className="nav_part" onClick={() => handleLinkClick('Resource')}>Resource</Nav.Link>
-          <Nav.Link as={Link} to="/contact" className="nav_part" onClick={() => handleLinkClick('Contact')}>Contact</Nav.Link>
-        </Nav>
-      </Offcanvas.Body>
-    </Offcanvas>
+      <Offcanvas show={showOffcanvas} onHide={() => setShowOffcanvas(false)}>
+        <Offcanvas.Header closeButton />
+        <Offcanvas.Body>
+          <Nav className="flex-column">
+            <Nav.Link as={Link} to="/" className="nav_part" onClick={() => handleLinkClick('Home')}>Home</Nav.Link>
+            <Nav.Link as={Link} to="/Resource" className="nav_part" onClick={() => handleLinkClick('Resource')}>Resource</Nav.Link>
+            <Nav.Link as={Link} to="/contact" className="nav_part" onClick={() => handleLinkClick('Contact')}>Contact</Nav.Link>
+          </Nav>
+        </Offcanvas.Body>
+      </Offcanvas>
     </>
   );
 };
 
 export default NavbarComponent;
-
-
-
-
-
-
-
-
